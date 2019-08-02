33372af40eb85e05010b06e40201cee8bc17cd84
unknown <myfaithfullife@gmail.com>
2019-08-02 11:51:11 +0800

    ini comit

---
+++ askterisk.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  2 10:04:56 2019
+
+@author: arriba
+"""
+
+
+        
+ for num in range(6):
+     for 1 in range(num):
+         print ('*', end=" ")
+     print("\n")
