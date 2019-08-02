9a37f09f68613c07b42169410bcc9273cbdb2fb7
unknown <myfaithfullife@gmail.com>
2019-08-02 13:41:13 +0800

    updated_test

--- askterisk.py
+++ askterisk.py
@@ -8,6 +8,8 @@ Created on Fri Aug  2 10:04:56 2019
 
         
  for num in range(6):
-     for 1 in range(num):
+    for 1 in range(num):
          print ('*', end=" ")
-     print("\n")
+         
+     print ("\n")
+     print ("hello")
